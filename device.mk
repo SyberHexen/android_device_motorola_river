31d986b935aaa0966743d432c5fa3938dfafd53d
Andrew Hexen <SyberHexen@gmail.com>
2024-12-01 07:34:13 -0800

    river: audio: add missing aov mixer paths
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I46da95ddeab9c0e17689ff0ca2a86edae81939f7

--- device.mk
+++ device.mk
@@ -33,6 +33,7 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/aov_ec_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/aov_ec_mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
